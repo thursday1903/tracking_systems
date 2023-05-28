acb546800975a33a4c079524290b5fa4c9afc9fe
vietlong1220 <vietlong1220@gmail.com>
2023-05-28 12:00:28 +0700

    add war file for tomcat

--- src/main/java/com/system/MainApplication.java
+++ src/main/java/com/system/MainApplication.java
@@ -27,11 +27,16 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
 @EnableScheduling
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MainApplication.class);
+    }
 
 	public static void main(String[] args) {
 		setTimeZone();
