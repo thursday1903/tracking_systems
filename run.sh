ee05ccea101f37ff0f9de88135cbd7d421c65392
Longdv20 <vietlong1220@gmail.com>
2023-05-28 19:20:02 +0700

    add Dockerfile

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ sudo docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=@123456A" -p 1433:1433 
 
 docker cp database_tracking.sql sql1:/file.sql
 
-docker exec -it sql1 /opt/mssql-tools/bin/sqlcmd -S localhost:1433 -U sa -P @123456A -i file.sql
+docker exec -it sql1 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P @123456A -i database_tracking.sql
