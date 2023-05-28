94920415ffa5634b41df3f2afaea65b7c9e16062
Longdv20 <vietlong1220@gmail.com>
2023-05-28 22:18:45 +0700

    release-1.0.0 final

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-sudo docker pull mcr.microsoft.com/mssql/server:2022-latest
+# sudo docker pull mcr.microsoft.com/mssql/server:2022-latest
 
-sudo docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=@123456A" -p 1433:1433 --name sql1 --hostname sql1 -d mcr.microsoft.com/mssql/server:2022-latest
+# sudo docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=@123456A" -p 1433:1433 --name sql1 --hostname sql1 -d mcr.microsoft.com/mssql/server:2022-latest
 
-docker cp database_tracking.sql sql1:/file.sql
+# docker cp database_tracking.sql sql1:/file.sql
 
-docker exec -it sql1 /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P @123456A -i database_tracking.sql
+docker exec -it sql_server /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P @123456A -i database_tracking.sql
